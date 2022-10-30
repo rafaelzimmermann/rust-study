4349836a1db58d14586fa986a5b93a6198571d37
Rafael Zimmermann <engzimmermann@gmail.com>
2022-10-30 13:07:44 +0100

    Use clap and derive feature to parse arguments
    Update README.md
    
    Include quote as dependency

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # RUST study project
+![image](https://user-images.githubusercontent.com/2369982/198877652-4ffa1586-8c7b-43d4-abed-1f26c271cc26.png)
 
-# Installation
+
+### Installation
 
 [Install Rust](https://www.rust-lang.org/tools/install)
 ```bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 ```
+
+### Using Cargo
+
+- Create project:  
+  `cargo new APP_NAME`
+- build:  
+  `cargo build`
+- run:  
+  `cargo run`  
+  `cargo run -- ARGS HERE`
+- test:  
+  `cargo test`
+
 
 
--- zecho/src/main.rs
+++ zecho/src/main.rs
@@ -1,9 +1,20 @@
-use clap::Command;
+use clap::{Parser};
+
+#[derive(Parser, Debug)]
+#[command(name = "zecho")]
+#[command(author = "Rafael Zimmermann <rafael@blackcoffee.page>")]
+#[command(version = "1.0")]
+#[command(about = "Display a line of text", long_about = None)]
+#[command(trailing_var_arg=true)]
+struct Arg {
+    #[arg(short, default_value_t = false)]
+    strip: bool,
+    #[arg(allow_hyphen_values=true)]
+    text: Vec<String>,
+}
 
 fn main() {
-    let _matches = Command::new("zecho")
-        .version("0.1.0")
-        .author("Rafael Zimmermann")
-        .about("Rust echo")
-        .get_matches();
+    let cli = Arg::parse();
+    let output = format!("{}", cli.text.join(" "));
+    println!("{}", output);
 }
